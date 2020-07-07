7b91339eb326dad4dba6b251f59da83a0f3cf034
Tanishq Krishnan <kronorak@outlook.com>
2020-07-07 19:06:23 +0530

    Added new branch to test and add new features
    The changes included in this commit invlude the following:
    -Added a battery_notif_v3 which doesn't check for steam process and
    hence acts as a proper battery notifier
    
    New features to include upon existing list as provided in the previous
    commit
    -Remove checking of i3 config

---
+++ battery_notif_v3.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+status=""
+capacity=""
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+msgID_1="99389281"
+msgID_2="99389282"
+
+get_battery_status() {
+	status="$(cat /sys/class/power_supply/BAT1/status)"
+	capacity="$(cat /sys/class/power_supply/BAT1/capacity)"
+}
+
+get_charging() {
+	cap="80"
+	
+	if [[ $capacity -gt $cap ]]; then
+		output="Current Battery Capacity:$capacity"
+		dunstify -a "Battery_Notify" -b "$output" -u normal -i $DIR/angel.png -h string:x-canonical-private-synchronous:"$msgID_1"
+	fi
+}
+
+get_discharging() {
+	cap="40"
+   	
+	if [[ $capacity -lt $cap ]]; then
+      		output="Current Battery Capacity:$capacity"
+      		dunstify -a "Battery_Notify" -b "$output" -u critical -i $DIR/death.png -h string:x-canonical-private-synchronous:"$msgID_1"
+	fi
+}
+
+if ! grep battery_notif.sh ~/.i3/config > /dev/null
+then	
+	echo "#Adding Battery Notification Execution" >> ~/.i3/config
+	echo "exec $DIR/battery_notif.sh" >> ~/.i3/config
+	echo "Patched Config"
+else
+	echo "Config already patched"
+fi
+
+while true
+do
+
+get_battery_status
+case "$status" in
+
+	"Full" | "Charging")
+	get_charging
+	;;
+	
+	"Discharging")
+	get_discharging
+	;;
+	
+	*)
+	echo "BUG"
+	;;	
+esac   	
+
+sleep 240
+done
